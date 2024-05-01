4534b11a40448fd5db790bce5f1289cf2ac5de3c
shogiai <kato-taka@kadokawa.jp>
2024-05-01 10:56:37 +0900

    16day

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ st.latex(r'''
      \sum_{k=0}^{n-1} ar^k =
      a \left(\frac{1-r^{n}}{1-r}\right)
      ''')
+
+number = st.sidebar.slider('Select a number:', 0, 10, 5)
+st.write('Selected number from slider widget is:', number)

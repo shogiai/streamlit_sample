d24d7e2d723051c4bc9fb5af81846c98f02196a7
shogiai <kato-taka@kadokawa.jp>
2024-05-01 11:19:35 +0900

    17day17day17day17day17day17day17day17day17day17day17day17day17day17day17day17day17day

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,16 @@
 import streamlit as st
 
-st.title('Customizing the theme of Streamlit apps')
+import streamlit as st
 
-st.write('Contents of the `.streamlit/config.toml` file of this app')
+# Everything is accessible via the st.secrets dict:
 
-st.code("""
-[theme]
-primaryColor="#F39C12"
-backgroundColor="#2E86C1"
-secondaryBackgroundColor="#AED6F1"
-textColor="#FFFFFF"
-font="monospace"
-""")
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
 
-number = st.sidebar.slider('Select a number:', 0, 10, 5)
-st.write('Selected number from slider widget is:', number)
+# And the root-level secrets are also accessible as environment variables:
+
+import os
+st.write(
+	"Has environment variables been set:",
+	os.environ["db_username"] == st.secrets["db_username"])
